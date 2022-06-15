973b80d24272f6737ee88c677535d7add8bfe925
simmasa <s.masa@hotmail.it>
2022-06-15 11:20:16 +0200

    Progetto completo

--- src/jana60/shop/Main.java
+++ src/jana60/shop/Main.java
@@ -1,4 +1,15 @@
 package jana60.shop;
 
 public class Main {
+    public static void main(String[] args) {
+        Prodotto primo = new Prodotto("nike","Scarpa", 90, 22);
+        Prodotto secondo = new Prodotto("amazon","echo", 120, 22);
+        Prodotto terzo = new Prodotto("ikea","libreria", 69, 22);
+        Prodotto quarto = new Prodotto("chanel","Profumo", 200, 22);
+        Prodotto quinto = new Prodotto("Mulino Bianco","Biscotti", 3.40, 10);
+
+        System.out.println(quinto.prezzoFormat());
+
+    }
+
 }
--- src/jana60/shop/Prodotto.java
+++ src/jana60/shop/Prodotto.java
@@ -1,4 +1,28 @@
 package jana60.shop;
 
+import java.text.DecimalFormat;
+
 public class Prodotto {
+
+    String marca;
+    String nome;
+    double prezzo;
+    double iva;
+
+    //Costruttore
+    Prodotto(String brand, String name, double price , double iva) {
+    marca = brand;
+    nome = name;
+    prezzo = price;
+    this.iva = iva;
+    }
+    //Methods
+    double prezzoFinale() {
+        return prezzo+((prezzo/100)*iva);
+    }
+    String prezzoFormat() {
+        DecimalFormat df = new DecimalFormat("#.00\u00A4");
+        String format = "Il prezzo di "+nome+" è "+df.format(prezzo) +" Aggiungendo l'iva diventa: "+df.format(prezzoFinale());
+        return format;
+    }
 }
